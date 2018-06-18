1374c7c6b12afc477af3e1e591d0716c5fe0269a
Terry Kim <terryucsb@gmail.com>
2018-06-17 17:10:06 -0700

    api correction

--- js/javascript.js
+++ js/javascript.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     $(this).addClass("active");
 
     var type = $(this).attr("data-type");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
     console.log(queryURL);
     $.ajax({
       url: queryURL,
